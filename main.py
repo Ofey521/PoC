53d0e368081cf71564cd81157d267cc3b07d2031
Mateusz Janczura <mat.janczura@gmail.com>
2022-12-04 17:01:58 +0100

    Rozbicie oraz refaktoryzacja

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import os, fnmatch
 
-print("Podaj ścieżkę do projektu")
-proj = input()
-print("Edycja pliku smd czy app?")
-smd_or_app = input()
-print("Co zmieniamy?")
-before = input()
-print("Na co zmieniamy?")
-after = input()
+accept = 'n'
 
+
+def entryQuestin():
+    proj = input("Podaj ścieżkę do projektów: \n")
+    smd_or_app = input("Edycja pliku smd czy app?\n")
+    before = input("Jaki fragment zmieniamy?\n")
+    after = input("Na co go zmieniamy?\n")
+    print("------------")
+    print(
+        'Podałeś następujące parametry:\nŚcieżka:{}\nEdycja pliku:{}\nCo zmieniamy:{}\nNa co zmieniamy:{}'.format(
+            proj, smd_or_app, before, after))
+    return proj, smd_or_app, before, after
+
+
+def showReplaceProject(proj):
+    print("\nZmiany zostaną naniesione w środowiskach: ")
+    for i in os.listdir(proj):
+        if i == ".DS_Store":
+            continue
+        else:
+            print(i)
+
+
+def checkCorrectParameters(proj, smd_or_app):
+    if not os.path.exists(proj):
+        print("Podałeś nieistniejącą ścieżkę")
+        return False
+    elif (smd_or_app != "smd") and (smd_or_app != "app"):
+        print("Zły parametr smd lub app")
+        return False
+    elif (before == ""):
+        print("Pusty parametr do zmiany")
+        return False
+    else:
+        return True
 
 
 def findReplace(directory, find, replace, filePattern):
@@ -21,15 +48,44 @@ def findReplace(directory, find, replace, filePattern):
             with open(filepath, "w") as f:
                 f.write(s)
 
-if(smd_or_app == "smd"):
-    for i in os.listdir(proj):
-        if (i == ".DS_Store"):
-            continue
-        findReplace("{}/{}/neos".format(proj, i), "{}".format(before), "{}".format(after), "*.smd")
-elif(smd_or_app == "app"):
-    for i in os.listdir(proj):
-        if (i == ".DS_Store"):
-            continue
-        findReplace("{}/{}/teneum_client".format(proj, i), "{}".format(before), "{}".format(after), "*.app")
+def changeEnvironment():
+    dropEnv = input("Podaj po spacji środowiska, w których nie dokonujesz zmian")
+    return dropEnv
+
+
+def replaceMethod(smd_or_app, accept):
+    if smd_or_app == "smd" and accept == "y":
+        for i in os.listdir(proj):
+            if i == ".DS_Store":
+                continue
+            findReplace("{}/{}/neos".format(proj, i), "{}".format(before), "{}".format(after), "*.smd")
+    elif smd_or_app == "app" and accept == "y":
+        for i in os.listdir(proj):
+            if i == ".DS_Store":
+                continue
+            findReplace("{}/{}/teneum_client".format(proj, i), "{}".format(before), "{}".format(after), "*.app")
+    else:
+        print("Niezaakceptowałeś zmian")
+
+
+proj, smd_or_app, before, after = entryQuestin()
+
+if checkCorrectParameters(proj,smd_or_app):
+    showReplaceProject(proj)
+    accept = input("Akceptujesz zmiany? [y/N]\n").lower()
 else:
-    print("Podałeś jakieś lipne wartości")
+    print("\nPodałeś złe parametry, spróbuj ponownie")
+    entryQuestin()
+
+change = input("Niezaakceptowałeś zmian, chcesz dokonać korekty?\n")
+if change:
+    print("Zmiany")
+    replace = changeEnvironment()
+    print("ShowReplace")
+
+    print("Accept")
+    print("replaceMethod")
+else:
+    print("Koniec")
+
+replaceMethod(smd_or_app, accept)
